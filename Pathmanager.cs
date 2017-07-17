648879d83ca9aeaedff035c2beb5e87d0c6360c5
Kibolabs <ileniusi52@gmail.com>
2017-07-17 09:29:03 -0500

    Add files via upload

---
+++ Pathmanager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pathmanager : MonoBehaviour {
+
+    Text textHints;
+    Text status;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.name=="Player")
+        {
+            if (PacketPickup.packetAcquired)
+            {
+
+            }
+
+        }
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            if (PacketPickup.packetAcquired)
+            {
+
+            }
+
+        }
+
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            if (PacketPickup.packetAcquired)
+            {
+
+            }
+
+        }
+    }
+}
